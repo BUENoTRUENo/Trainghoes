ae95c47a0174e1ceab66b6e253a3645815bb7ef1
Rony <roncam88@gmail.com>
2020-10-28 19:00:41 -0700

    logic attempt

--- app/src/main/java/com/ron/dojotraining/MainActivity.kt
+++ app/src/main/java/com/ron/dojotraining/MainActivity.kt
@@ -3,7 +3,11 @@ package com.ron.dojotraining
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +16,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val loginButton:Button = findViewById(R.id.fuckinbutton)
+        val flippyswitch :Switch =findViewById(R.id.toddlelearn)
+        val radsrads :RadioButton =findViewById(R.id.radswitchy)
+        val checkychecky :CheckBox = findViewById(R.id.checkingshitout)
+
+          flippyswitch.setOnCheckedChangeListener { buttonView, isChecked ->
+              Log.d("here","blahblahblah $isChecked")
+              if (isChecked){
+                  title="This Shit Works"
+              } else{
+                  title="This Shit Dont Work"
+              }
+          }
         loginButton.setOnClickListener {
+
             startActivity(Intent(this,SecondActivity::class.java))
+
         }
     }
 
